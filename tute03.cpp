6d7038c50d1580642cb4c42d9d48ec50f27f3757
IT21267772 <95214934+IT21267772@users.noreply.github.com>
2022-02-21 08:09:12 +0000

    Exercise 3 completed

--- tute03.cpp
+++ tute03.cpp
@@ -4,6 +4,7 @@ Convert the C program given below which calculates the Factorial of a number tha
 
 Please Note that the input command in C++ is std::cin. This is a representation of the Keyboard.*/
 
+
 #include <iostream>
 int main()
 {
